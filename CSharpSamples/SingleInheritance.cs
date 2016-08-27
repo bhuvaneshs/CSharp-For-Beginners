c619375b539e4e97c0439dac7063ee95333f8050
Bhuvanesh <blazingbhuvan@gmail.com>
2016-08-27 20:39:49 +0530

    single inheritance

---
+++ CSharpSamples/SingleInheritance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpSamples
+{
+    class SingleInheritance
+    {
+        public static void Main()
+        {
+            Rectangle rect = new Rectangle();
+            rect.setWidth(5);
+            rect.setHeight(5);
+
+            Console.WriteLine("The area of a rectangle is :" + rect.getArea());
+            Console.ReadLine();
+        }
+    }
+
+    public class Shape
+    {
+        protected int width;
+        protected int height;
+
+        public void setWidth(int w)
+        {
+            width = w;
+        }
+        public void setHeight(int h)
+        {
+            height = h;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public int getArea()
+        {
+            return (width * height);
+        }
+    }
+}
